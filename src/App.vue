57dbd739061e56df8123507f1ea2374097d0204f
39340 <iacopo.cutino@gmail.com>
2024-08-31 10:40:16 +0200

    primo commit.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="pt-4">Weather Web</h1>
+    <p>Weather forecast for a location</p>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
