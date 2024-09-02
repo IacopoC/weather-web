7ddf0b8fbeda9088ceeda907b6bd4564b8875372
39340 <iacopo.cutino@gmail.com>
2024-09-02 21:49:19 +0200

    aggiunto Footer.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
+  <main class="flex-shrink-0">
   <div class="container">
     <Header title="Weather Web"></Header>
+    <p>Content</p>
   </div>
+  </main>
+  <Footer></Footer>
 </template>
 
 <style lang="scss">
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      Year: new Date().getFullYear()
+    };
+  }
+}
+</script>
+
+<template>
+  <footer class="footer mt-auto py-3 bg-body-tertiary">
+    <div class="container">
+      <span class="text-body-secondary">{{ Year }} © - Weather Web</span>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
