35fa1c6ca5d35fdc06ccd8045f8d3f452ca8ce72
39340 <iacopo.cutino@gmail.com>
2024-09-03 21:31:50 +0200

    aggiunto navigation.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <script>
+import Navigation from "@/components/Navigation.vue";
 export default {
   name: "Header",
+  components: {Navigation},
   props: {
     title: String
   }
@@ -12,13 +14,7 @@ export default {
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <span class="fs-4">{{ title }}</span>
       </a>
-
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Weather today</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Weather week</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Weather info</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact us</a></li>
-      </ul>
+      <Navigation></Navigation>
     </header>
 </template>
 
---
+++ src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Navigation"
+}
+</script>
+
+<template>
+  <ul class="nav nav-pills">
+    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Weather today</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Weather week</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Weather info</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Contact us</a></li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+
+</style>
