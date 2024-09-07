2dcf410f46f25e036ed6092505ad5c260ebb10ee
39340 <iacopo.cutino@gmail.com>
2024-09-07 14:18:46 +0200

    aggiunto banner quote.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import BannerQuote from "@/components/BannerQuote.vue";
 import WeatherToday from "@/components/weather/WeatherToday.vue";
 </script>
 
@@ -9,20 +10,9 @@ import WeatherToday from "@/components/weather/WeatherToday.vue";
   <div class="container">
     <Header title="Weather Web"></Header>
     <WeatherToday></WeatherToday>
-    <em>“Conversation about the weather is the last refuge of the unimaginative.”</em>
-    <p><strong>Oscar Wilde</strong></p>
     <p><a href="https://open-meteo.com/">Free Weather API</a></p>
   </div>
-    <div class="my-5">
-      <div class="p-5 text-center bg-body-tertiary">
-        <div class="container py-5">
-          <em class="col-lg-8 mx-auto lead">
-            “Conversation about the weather is the last refuge of the unimaginative.”
-            <br><strong>Oscar Wilde</strong>
-          </em>
-        </div>
-      </div>
-    </div>
+    <BannerQuote></BannerQuote>
   </main>
   <Footer></Footer>
 </template>
---
+++ src/components/BannerQuote.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "BannerQuote"
+}
+</script>
+
+<template>
+  <div class="my-5">
+    <div class="p-5 text-center bg-body-tertiary">
+      <div class="container py-5">
+        <em class="col-lg-8 mx-auto lead">
+          “Conversation about the weather is the last refuge of the unimaginative.”
+          <br><strong>Oscar Wilde</strong>
+        </em>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
