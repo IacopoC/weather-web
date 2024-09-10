928560b953acd9b18657bd035d74673fa72fd827
39340 <iacopo.cutino@gmail.com>
2024-09-10 21:46:33 +0200

    modifica font stile.

--- src/components/weather/WeatherToday.vue
+++ src/components/weather/WeatherToday.vue
@@ -56,34 +56,34 @@ export default {
      <h5>Weather Today</h5>
      <div class="pt-4 row">
        <div class="col-lg-4">
-         <i class="bi bi-brightness-high-fill fs-2"></i>
+         <i class="bi bi-brightness-high-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature"></i>
          <p class="pt-4"><strong>Temperature:</strong></p>
          <p>{{ temperature }} {{ temperature_unit }}</p>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
-         <i class="bi bi-thermometer-half fs-2"></i>
+         <i class="bi bi-thermometer-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature perception"></i>
          <p class="pt-4"><strong>Temperature perception:</strong></p>
          <p>{{ ap_temperature }} {{ ap_temperature_unit }}</p>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
-         <i class="bi bi-droplet-half fs-2"></i>
+         <i class="bi bi-droplet-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual humidity"></i>
          <p class="pt-4"><strong>Humidity:</strong></p>
          <p>{{ humidity }} {{ humidity_unit }}</p>
        </div><!-- /.col-lg-4 -->
        </div>
      <div class="row pt-4">
        <div class="col-lg-4">
-         <i class="bi bi-wind fs-2"></i>
+         <i class="bi bi-wind fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual wind speed"></i>
          <p class="pt-4"><strong>Wind speed:</strong></p>
          <p>{{ wind }} {{ wind_unit }}</p>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
-         <i class="bi bi-cloud-drizzle-fill fs-2"></i>
+         <i class="bi bi-cloud-drizzle-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual rain"></i>
          <p class="pt-4"><strong>Rain:</strong></p>
          <p>{{ rain }} {{ rain_unit }}</p>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
-         <i class="bi bi-bullseye fs-2"></i>
+         <i class="bi bi-bullseye fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual visibility"></i>
          <p class="pt-4"><strong>Visibility:</strong></p>
          <p>{{ visibility }} {{ visibility_unit }}</p>
        </div><!-- /.col-lg-4 -->
@@ -95,5 +95,9 @@ export default {
 </template>
 
 <style scoped lang="scss">
+
+i {
+  color: lightblue;
+}
 
 </style>
