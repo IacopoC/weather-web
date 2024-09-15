f7b3eb37e4948ad892f47551a5eb7c38f332fbe0
39340 <iacopo.cutino@gmail.com>
2024-09-15 21:01:52 +0200

    modifica commento.

--- src/components/weather/WeatherToday.vue
+++ src/components/weather/WeatherToday.vue
@@ -78,44 +78,44 @@ export default {
          <i class="bi bi-brightness-high-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature"></i>
          <p class="pt-4"><strong>Temperature:</strong></p>
          <h4>{{ temperature }} {{ temperature_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        <div class="col-lg-3">
          <i class="bi bi-thermometer-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature perception"></i>
          <p class="pt-4"><strong>Temperature perception:</strong></p>
          <h4>{{ ap_temperature }} {{ ap_temperature_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        <div class="col-lg-3">
          <i class="bi bi-droplet-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual humidity"></i>
          <p class="pt-4"><strong>Humidity:</strong></p>
          <h4>{{ humidity }} {{ humidity_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        <div class="col-lg-3">
          <i class="bi bi-snow fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual snowfall"></i>
          <p class="pt-4"><strong>Snowfall:</strong></p>
          <h4>{{ snow }} {{ snow_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        </div>
      <div class="row pt-4">
        <div class="col-lg-3">
          <i class="bi bi-wind fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual wind speed"></i>
          <p class="pt-4"><strong>Wind speed:</strong></p>
          <h4>{{ wind }} {{ wind_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        <div class="col-lg-3">
          <i class="bi bi-cloud-drizzle-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual rain"></i>
          <p class="pt-4"><strong>Rain:</strong></p>
          <h4>{{ rain }} {{ rain_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        <div class="col-lg-3">
          <i class="bi bi-bullseye fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual visibility"></i>
          <p class="pt-4"><strong>Visibility:</strong></p>
          <h4>{{ visibility }} {{ visibility_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
        <div class="col-lg-3">
          <i class="bi bi-cloud-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual cloud coverage"></i>
          <p class="pt-4"><strong>Clouds:</strong></p>
          <h4>{{ cloud }} {{ cloud_unit }}</h4>
-       </div><!-- /.col-lg-4 -->
+       </div><!-- /.col-lg-3 -->
      </div>
      <div v-if="hasError">
        <p>Error in displaying weather data. Try again later.</p>
