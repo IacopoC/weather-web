abc534cb5df61d52953fffdb8c360d3211af6315
39340 <iacopo.cutino@gmail.com>
2024-09-15 20:36:44 +0200

    modifica carattere.

--- src/components/weather/WeatherToday.vue
+++ src/components/weather/WeatherToday.vue
@@ -69,47 +69,43 @@ export default {
        <div class="col-lg-4">
          <i class="bi bi-brightness-high-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature"></i>
          <p class="pt-4"><strong>Temperature:</strong></p>
-         <p>{{ temperature }} {{ temperature_unit }}</p>
+         <h4>{{ temperature }} {{ temperature_unit }}</h4>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
          <i class="bi bi-thermometer-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature perception"></i>
          <p class="pt-4"><strong>Temperature perception:</strong></p>
-         <p>{{ ap_temperature }} {{ ap_temperature_unit }}</p>
+         <h4>{{ ap_temperature }} {{ ap_temperature_unit }}</h4>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
          <i class="bi bi-droplet-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual humidity"></i>
          <p class="pt-4"><strong>Humidity:</strong></p>
-         <p>{{ humidity }} {{ humidity_unit }}</p>
+         <h4>{{ humidity }} {{ humidity_unit }}</h4>
        </div><!-- /.col-lg-4 -->
        </div>
      <div class="row pt-4">
        <div class="col-lg-4">
          <i class="bi bi-wind fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual wind speed"></i>
          <p class="pt-4"><strong>Wind speed:</strong></p>
-         <p>{{ wind }} {{ wind_unit }}</p>
+         <h4>{{ wind }} {{ wind_unit }}</h4>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
          <i class="bi bi-cloud-drizzle-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual rain"></i>
          <p class="pt-4"><strong>Rain:</strong></p>
-         <p>{{ rain }} {{ rain_unit }}</p>
+         <h4>{{ rain }} {{ rain_unit }}</h4>
        </div><!-- /.col-lg-4 -->
        <div class="col-lg-4">
          <i class="bi bi-bullseye fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual visibility"></i>
          <p class="pt-4"><strong>Visibility:</strong></p>
-         <p>{{ visibility }} {{ visibility_unit }}</p>
+         <h4>{{ visibility }} {{ visibility_unit }}</h4>
        </div><!-- /.col-lg-4 -->
      </div>
      <div v-if="hasError">
-       Error in displaying weather data. Try again later.
+       <p>Error in displaying weather data. Try again later.</p>
      </div>
    </div>
   <GetLocation @location-updated="onLocationUpdated"></GetLocation>
 </template>
 
 <style scoped lang="scss">
-
-i {
-  color: lightblue;
-}
 
 </style>
