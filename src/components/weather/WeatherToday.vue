2cedb245e521c868c57034cc8d9568604f84d78d
39340 <iacopo.cutino@gmail.com>
2024-09-15 20:58:12 +0200

    aggiunti dati meteo.

--- src/components/weather/WeatherToday.vue
+++ src/components/weather/WeatherToday.vue
@@ -13,6 +13,10 @@ export default {
       temperature: '',
       ap_temperature: '',
       rain:'',
+      snow:'',
+      snow_unit:'',
+      cloud:'',
+      cloud_unit:'',
       visibility:'',
       humidity:'',
       wind: '',
@@ -32,7 +36,7 @@ export default {
     },
     fetchWeatherData(latitude, longitude) {
       this.isLoading = true;
-      fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,rain,visibility,wind_speed_10m`)
+      fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,rain,visibility,wind_speed_10m,snowfall,cloud_cover`)
           .then(response => response.json())
           .then(data => {
             this.temperature = data.current['temperature_2m'];
@@ -41,6 +45,8 @@ export default {
             this.humidity = data.current['relative_humidity_2m'];
             this.rain = data.current['rain'];
             this.visibility = data.current['visibility'];
+            this.snow = data.current['snowfall'];
+            this.cloud = data.current['cloud_cover'];
 
             this.temperature_unit = data.current_units['temperature_2m'];
             this.wind_unit = data.current_units['wind_speed_10m'];
@@ -48,6 +54,8 @@ export default {
             this.humidity_unit = data.current_units['relative_humidity_2m'];
             this.rain_unit = data.current_units['rain'];
             this.visibility_unit = data.current_units['visibility'];
+            this.snow_unit = data.current_units['snowfall'];
+            this.cloud_unit = data.current_units['cloud_cover'];
           })
           .catch(error => {
             console.error('Sorry, an error occurred:', error);
@@ -66,37 +74,47 @@ export default {
    <div v-else>
      <h5>Weather Today</h5>
      <div class="pt-4 row">
-       <div class="col-lg-4">
+       <div class="col-lg-3">
          <i class="bi bi-brightness-high-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature"></i>
          <p class="pt-4"><strong>Temperature:</strong></p>
          <h4>{{ temperature }} {{ temperature_unit }}</h4>
        </div><!-- /.col-lg-4 -->
-       <div class="col-lg-4">
+       <div class="col-lg-3">
          <i class="bi bi-thermometer-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual temperature perception"></i>
          <p class="pt-4"><strong>Temperature perception:</strong></p>
          <h4>{{ ap_temperature }} {{ ap_temperature_unit }}</h4>
        </div><!-- /.col-lg-4 -->
-       <div class="col-lg-4">
+       <div class="col-lg-3">
          <i class="bi bi-droplet-half fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual humidity"></i>
          <p class="pt-4"><strong>Humidity:</strong></p>
          <h4>{{ humidity }} {{ humidity_unit }}</h4>
        </div><!-- /.col-lg-4 -->
+       <div class="col-lg-3">
+         <i class="bi bi-snow fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual snowfall"></i>
+         <p class="pt-4"><strong>Snowfall:</strong></p>
+         <h4>{{ snow }} {{ snow_unit }}</h4>
+       </div><!-- /.col-lg-4 -->
        </div>
      <div class="row pt-4">
-       <div class="col-lg-4">
+       <div class="col-lg-3">
          <i class="bi bi-wind fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual wind speed"></i>
          <p class="pt-4"><strong>Wind speed:</strong></p>
          <h4>{{ wind }} {{ wind_unit }}</h4>
        </div><!-- /.col-lg-4 -->
-       <div class="col-lg-4">
+       <div class="col-lg-3">
          <i class="bi bi-cloud-drizzle-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual rain"></i>
          <p class="pt-4"><strong>Rain:</strong></p>
          <h4>{{ rain }} {{ rain_unit }}</h4>
        </div><!-- /.col-lg-4 -->
-       <div class="col-lg-4">
+       <div class="col-lg-3">
          <i class="bi bi-bullseye fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual visibility"></i>
          <p class="pt-4"><strong>Visibility:</strong></p>
          <h4>{{ visibility }} {{ visibility_unit }}</h4>
+       </div><!-- /.col-lg-4 -->
+       <div class="col-lg-3">
+         <i class="bi bi-cloud-fill fs-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Actual cloud coverage"></i>
+         <p class="pt-4"><strong>Clouds:</strong></p>
+         <h4>{{ cloud }} {{ cloud_unit }}</h4>
        </div><!-- /.col-lg-4 -->
      </div>
      <div v-if="hasError">
