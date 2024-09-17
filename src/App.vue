90b98c6cc1785f23e2787490167e5c7584ef7a4c
39340 <iacopo.cutino@gmail.com>
2024-09-17 20:55:31 +0200

    aggiunto weather weekly.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 import BannerQuote from "@/components/BannerQuote.vue";
 import WeatherToday from "@/components/weather/WeatherToday.vue";
+import WeatherWeekly from "@/components/weather/WeatherWeekly.vue";
 </script>
 
 <template>
@@ -12,6 +13,9 @@ import WeatherToday from "@/components/weather/WeatherToday.vue";
     <WeatherToday></WeatherToday>
     <p><strong>Note:</strong> Current conditions are based on 15-minutely weather data.</p>
   </div>
+    <div class="container">
+      <WeatherWeekly></WeatherWeekly>
+    </div>
     <BannerQuote></BannerQuote>
   </main>
   <Footer></Footer>
---
+++ src/components/weather/WeatherWeekly.vue
@@ -0,0 +1,32 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default {
+  name: "WeatherWeekly",
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#c3c3c3',
+            data: [40, 20, 12]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h5 class="pt-4">Weather Weekly</h5>
+  <Bar :data="chartData" />
+</template>
+
+<style scoped lang="scss">
+
+</style>
