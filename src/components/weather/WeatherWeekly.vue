54b849b751596ea6ec1fbfebad374f96960812e7
39340 <iacopo.cutino@gmail.com>
2024-09-19 19:39:25 +0200

    bozza 2 script grafico.

--- src/components/weather/WeatherWeekly.vue
+++ src/components/weather/WeatherWeekly.vue
@@ -1,19 +1,25 @@
 <script>
-import { Line } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale } from 'chart.js'
-ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale)
-export default {
+import { Line } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale,
+  LinearScale } from 'chart.js';
+ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale);
+
+export
+default {
   name: 'WeatherWeekly',
   components: { Line },
-  data: () => ({
-    loaded: false,
-    chartData: null
-  }),
-  async mounted () {
-    this.loaded = false
+  data() {
+    return {
+      loaded: false,
+      chartData: null
+    };
+  },
+  async mounted() {
+    this.loaded = false;
 
     try {
-      const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
+      const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m');
+
       const weatherData = await response.json();
 
       const labels = weatherData.hourly.time;
@@ -29,12 +35,12 @@ export default {
         }]
       };
 
-      this.loaded = true
+      this.loaded = true;
     } catch (error) {
       console.error('Sorry, an error occurred:', error);
     }
   }
-}
+};
 </script>
 
 <template>
