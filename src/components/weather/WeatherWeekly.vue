63ae8a59602fb4df537be24120f64a85a1d2b9ec
39340 <iacopo.cutino@gmail.com>
2024-09-21 21:06:26 +0200

    primo script grafico completo.

--- src/components/weather/WeatherWeekly.vue
+++ src/components/weather/WeatherWeekly.vue
@@ -1,8 +1,8 @@
 <script>
 import { Line } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale,
-  LinearScale } from 'chart.js';
-ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale);
+  LinearScale, PointElement } from 'chart.js';
+ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
 
 export
 default {
