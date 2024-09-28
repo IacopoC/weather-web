d28797a0a9c67da64251ef824ce3fe850aefbca7
39340 <iacopo.cutino@gmail.com>
2024-09-28 19:50:38 +0200

    aggiunte citazioni quotidiane.

--- src/components/BannerQuote.vue
+++ src/components/BannerQuote.vue
@@ -1,6 +1,25 @@
 <script>
 export default {
-  name: "BannerQuote"
+  name: "BannerQuote",
+  data() {
+    return {
+      texts: [
+        { quote: "“All weather is sin-related. Lust causes thunder, anger causes fog, and you don’t want to know what causes dew.”", author: "Stephen Colbert" },
+        { quote: "“Going to the woods is going home, for I suppose we came from the woods originally. But in some of nature’s forests, the adventurous traveler seems a feeble, unwelcome creature; wild beasts and the weather trying to kill him, the rank, tangled vegetation, armed with spears and stinging needles, barring his way and making life a hard struggle.”", author: "John Muir" },
+        { quote: "“If enough people think of a thing and work hard enough at it, I guess it’s pretty nearly bound to happen, wind and weather permitting.”", author: "Laura Ingalls Wilder" },
+        { quote: "“I don’t have to get up in the morning and go beat up my body like I used to. I don’t have to be out there in August in 108 degree weather down in Texas.”", author: "Emmitt Smith" },
+        { quote: "“Climate is what we expect, weather is what we get.”", author: "Mark Twain" },
+        { quote: "“All the world lies warm in one heart, yet the Sierra seems to get more light than other mountains. The weather is mostly sunshine embellished with magnificent storms, and nearly everything shines from base to summit – the rocks, streams, lakes, glaciers, irised falls, and the forests of silver fir and silver pine.”", author: "John Muir" },
+        { quote: "“In the Spring, I have counted 136 different kinds of weather inside of 24 hours.”", author: "Mark Twain" }
+      ]
+    };
+  },
+  computed: {
+    dailyQuote() {
+      const today = new Date().getDay();
+      return this.texts[today];
+    }
+  }
 }
 </script>
 
@@ -8,9 +27,10 @@ export default {
   <div class="my-5">
     <div class="p-5 text-center bg-body-tertiary">
       <div class="container py-5">
+        <h2 class="lead pb-2"><strong>Quote of the day:</strong></h2>
         <em class="col-lg-8 mx-auto lead text-dark-emphasis">
-          “Conversation about the weather is the last refuge of the unimaginative.”
-          <br><strong>Oscar Wilde</strong>
+          {{ dailyQuote.quote }}<br>
+          <strong>{{ dailyQuote.author }}</strong>
         </em>
       </div>
     </div>
