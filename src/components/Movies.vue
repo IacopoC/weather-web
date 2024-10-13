71c909a311e397b6606205f77a8d87b5413c1afa
39340 <iacopo.cutino@gmail.com>
2024-10-13 19:05:58 +0200

    aggiunti dati componente Movies.

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -11,11 +11,41 @@ export default {
     <div class="row">
       <div class="col-md-3">
         <div class="card">
-          <img src="#" class="card-img-top" alt="...">
+          <img src="@/assets/img/twister.jpg" class="card-img-top" alt="twister">
           <div class="card-body">
             <h5 class="card-title">Twister</h5>
             <p class="card-text">USA - 1996 | In Twister, Bill and Jo Harding, advanced storm chasers on the brink of divorce, must join together to create an advanced...</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Discover more</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="@/assets/img/geostorm.jpg" class="card-img-top" alt="geostorm">
+          <div class="card-body">
+            <h5 class="card-title">Geostorm</h5>
+            <p class="card-text">USA - 2017 | Geostorm is a sci-fi thriller starring Gerard Butler as satellite designer Jake Lawson who discovers a problem with an...</p>
+            <a href="#" class="btn btn-primary">Discover more</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="@/assets/img/day-after-tomorrow.jpg" class="card-img-top" alt="the day after tomorrow">
+          <div class="card-body">
+            <h5 class="card-title">The Day After Tomorrow</h5>
+            <p class="card-text">USA - 2004 | The Day After Tomorrow depicts the cataclysmic effects of global climate change as a series of extreme weather events unfold...</p>
+            <a href="#" class="btn btn-primary">Discover more</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="@/assets/img/greenland.jpg" class="card-img-top" alt="greenland">
+          <div class="card-body">
+            <h5 class="card-title">Greenland</h5>
+            <p class="card-text">USA - 2020 | Greenland is a 2020 thriller directed by Ric Roman Waugh and written by Chris Sparling. Starring Gerard Butler, Scott Glenn...</p>
+            <a href="#" class="btn btn-primary">Discover more</a>
           </div>
         </div>
       </div>
