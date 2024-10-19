5f203add4bab67c1b355e398f54a248c30d9835b
39340 <iacopo.cutino@gmail.com>
2024-10-19 22:19:38 +0200

    bozza loop Movies 3.

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -8,33 +8,37 @@ export default {
           title: 'Twister',
           year: 1996,
           location: 'USA',
-          image: '/assets/img/twister.jpg',
+          image: 'twister',
           description: 'In Twister, Bill and Jo Harding, advanced storm chasers on the brink of divorce, must join together to create an advanced...',
         },
         {
           title: 'Geostorm',
           year: 2017,
           location: 'USA',
-          image: '/assets/img/geostorm.jpg',
+          image: 'geostorm',
           description: 'Geostorm is a sci-fi thriller starring Gerard Butler as satellite designer Jake Lawson who discovers a problem with an...',
         },
         {
           title: 'The Day After Tomorrow',
           year: 2004,
           location: 'USA, UK',
-          image: '/assets/img/day-after-tomorrow.jpg',
+          image: 'day-after-tomorrow',
           description: 'The Day After Tomorrow depicts the cataclysmic effects of global climate change as a series of extreme weather events unfold...',
         },
         {
           title: 'Greenland',
           year: 2020,
           location: 'USA',
-          image: '/assets/img/greenland.jpg',
+          image: 'greenland',
           description: 'Greenland is a 2020 thriller directed by Ric Roman Waugh and written by Chris Sparling. Starring Gerard Butler, Scott Glenn...',
         },
       ]
     }
-  }
+  },
+  methods:
+     function getImageUrl(name) {
+          return new URL(`./assets/img/${name}.jpg`, import.meta.url).href
+     }
 }
 </script>
 
@@ -45,7 +49,7 @@ export default {
     <div class="row">
       <div class="col-md-3" v-for="movie in movies" :key="movie.title">
         <div class="card">
-          <img :src="movie.image" class="card-img-top" :alt="movie.title">
+          <img :src="getImageUrl(movie.image)" class="card-img-top" :alt="movie.title">
           <div class="card-body">
             <h5 class="card-title">{{ movie.title }}</h5>
             <p class="card-text">{{ movie.location }} - {{ movie.year }} | {{ movie.description }}</p>
