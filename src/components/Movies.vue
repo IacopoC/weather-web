9a1bcf21d80b4c29c4c295f3944d530cd82292a9
39340 <iacopo.cutino@gmail.com>
2024-10-27 10:41:15 +0100

    aggiunto film e modifica dati.

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -9,7 +9,7 @@ export default {
           year: 1996,
           location: 'USA',
           image: 'twister',
-          description: 'In Twister, Bill and Jo Harding, advanced storm chasers on the brink of divorce, must join together to create an advanced...',
+          duration:'1h 53min',
           imdb_link:'tt0117998',
         },
         {
@@ -17,7 +17,7 @@ export default {
           year: 2017,
           location: 'USA',
           image: 'geostorm',
-          description: 'Geostorm is a sci-fi thriller starring Gerard Butler as satellite designer Jake Lawson who discovers a problem with an...',
+          duration:'1h 49min',
           imdb_link:'tt1981128',
         },
         {
@@ -25,7 +25,7 @@ export default {
           year: 2004,
           location: 'USA, UK',
           image: 'day-after-tomorrow',
-          description: 'The Day After Tomorrow depicts the cataclysmic effects of global climate change as a series of extreme weather events unfold...',
+          duration:'2h 4min',
           imdb_link:'tt0319262',
         },
         {
@@ -33,8 +33,16 @@ export default {
           year: 2020,
           location: 'USA',
           image: 'greenland',
-          description: 'Greenland is a 2020 thriller directed by Ric Roman Waugh and written by Chris Sparling. Starring Gerard Butler, Scott Glenn...',
+          duration:'1h 59min',
           imdb_link:'tt7737786',
+        },
+        {
+          title: 'Into the Storm',
+          year: 2014,
+          location: 'USA',
+          image: 'into-the-storm',
+          duration:'1h 29min',
+          imdb_link:'tt2106361',
         },
       ]
     }
@@ -53,12 +61,12 @@ export default {
   <p class="px-2 pb-4">A selection of movies about weather of different genres.</p>
   <div class="container">
     <div class="row">
-      <div class="col-md-3" v-for="movie in movies" :key="movie.title">
+      <div class="col-lg" v-for="movie in movies" :key="movie.title">
         <div class="card">
           <img :src="getImageUrl(movie.image)" class="card-img-top" :alt="movie.title">
           <div class="card-body">
             <h5 class="card-title">{{ movie.title }}</h5>
-            <p class="card-text">{{ movie.location }} - {{ movie.year }} | {{ movie.description }}</p>
+            <p class="card-text">{{ movie.location }} - {{ movie.year }} | {{ movie.duration }}</p>
             <a :href="'https://www.imdb.com/title/' + movie.imdb_link + '/'" class="btn btn-primary">About the Movie <i class="bi bi-box-arrow-up-right p-2"></i></a>
           </div>
         </div>
