a47e28ea750a8a6121030aa6024be6cf271cfcca
39340 <iacopo.cutino@gmail.com>
2024-11-10 21:46:31 +0100

    modifica marginature.

--- src/components/BannerHero.vue
+++ src/components/BannerHero.vue
@@ -8,7 +8,8 @@ export default {
   <div class="px-4 pt-5 mt-5 text-center">
     <h1 class="display-5 pt-5 fw-bold text-body-emphasis">Welcome to Weather Web!</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Weather for your location and other info about weather forecast. Discover a list of quotes and movies about weather!</p>
+      <p class="lead pt-3">Weather for your location and other info about weather forecast.</p>
+      <p class="lead mb-4">Discover a list of quotes and movies about weather!</p>
     </div>
   </div>
 </template>
