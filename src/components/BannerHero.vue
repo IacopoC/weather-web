a70aeeeec130b586a93e796a46d15421cbf600aa
39340 <iacopo.cutino@gmail.com>
2024-11-13 18:07:49 +0100

    aggiunti link anchor.

--- src/components/BannerHero.vue
+++ src/components/BannerHero.vue
@@ -22,7 +22,7 @@ export default {
     <h1 class="display-5 pt-5 fw-bold text-body-emphasis">Welcome to Weather Web!</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead pt-3">Weather for your location and other info about weather forecast.</p>
-      <p class="lead mb-4">Discover a list of quotes and movies about weather!</p>
+      <p class="lead mb-4">Discover a list of <a class="text-white" href="#weatherquotes">quotes</a> and <a class="text-white" href="#weathermovies">movies</a> about weather!</p>
       <i class="bi bi-three-dots"></i>
       <GetLocation @location-error="onLocationError"></GetLocation>
     </div>
