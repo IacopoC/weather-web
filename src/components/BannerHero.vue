4de91eb70ddc79ab2b8854bf20c5b537507466b0
39340 <iacopo.cutino@gmail.com>
2024-11-14 21:08:57 +0100

    aggiunto stile titolo, frammenti.

--- src/components/BannerHero.vue
+++ src/components/BannerHero.vue
@@ -20,10 +20,10 @@ export default {
 
 <template>
   <div class="px-4 pt-5 mt-5 text-center">
-    <h1 class="display-5 pt-5 fw-bold text-body-emphasis">Welcome to Weather Web!</h1>
+    <h1 class="display-5 pt-5 fw-bold text-body-emphasis head-text">Welcome to Weather Web!</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead pt-3">Weather for your location and other info about weather forecast.</p>
-      <p class="lead mb-4">Discover a list of <a class="text-white" href="#weatherquotes">quotes</a> and <a class="text-white" href="#weathermovies">movies</a> about weather!</p>
+      <p class="lead mb-4">Discover a list of <a class="text-white" href="/#:~:text=quote">quotes</a> and <a class="text-white" href="/#:~:text=genres">movies</a> about weather!</p>
       <i class="bi bi-three-dots"></i>
       <GetLocation @location-error="onLocationError"></GetLocation>
       <div v-if="locationError">
@@ -34,5 +34,11 @@ export default {
 </template>
 
 <style scoped lang="scss">
-
+ .head-text {
+   font-size: 3em;
+   background: -webkit-linear-gradient(rgb(0,0,255), 	rgb(30,144,255));
+   background-clip: text;
+   -webkit-background-clip: text;
+   -webkit-text-fill-color: transparent;
+ }
 </style>
