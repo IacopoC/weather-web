b595e09e6b2c948fb327fbf6be0e1324a23b3e61
39340 <iacopo.cutino@gmail.com>
2025-01-22 18:00:16 +0100

    aggiunto componente.

---
+++ src/components/weather/WeatherCard.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "WeatherCard",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+    threshold: {
+      type: Number,
+      default: null,
+    },
+    thresholdClass: {
+      type: String,
+      default: null,
+    },
+    tooltip: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="col-lg-3">
+    <div
+        class="shadow-lg p-3 mb-5 bg-body-tertiary rounded" :class="thresholdClass && value > threshold ? thresholdClass : ''">
+      <i :class="icon + ' fs-1'" data-bs-toggle="tooltip" data-bs-placement="top" :title="tooltip"></i>
+      <p class="pt-4"><strong>{{ label }}:</strong></p>
+      <h4>{{ value }} {{ unit }}</h4>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
