d8cfb76898d4aae3843b6f4e87b886d934f446d2
39340 <iacopo.cutino@gmail.com>
2025-01-25 17:59:08 +0100

    completato loop weather card.

--- src/components/weather/WeatherToday.vue
+++ src/components/weather/WeatherToday.vue
@@ -30,14 +30,14 @@ export default {
       visibility_unit:'',
       humidity_unit:'',
       weatherData: [
-        { icon: 'bi bi-brightness-high-fill', label: 'Temperature', value: this.temperature, unit: this.temperature_unit, threshold: 17, thresholdClass: 'bg-warning-subtle', tooltip: 'Actual temperature' },
-        { icon: 'bi bi-thermometer-half', label: 'Temperature perception', value: this.ap_temperature, unit: this.ap_temperature_unit, threshold: 17, thresholdClass: 'bg-warning-subtle', tooltip: 'Actual temperature perception' },
-        { icon: 'bi bi-droplet-half', label: 'Humidity', value: this.humidity, unit: this.humidity_unit, threshold: 70, thresholdClass: 'bg-primary-subtle', tooltip: 'Actual humidity' },
-        { icon: 'bi bi-snow', label: 'Snowfall', value: this.snow, unit: this.snow_unit, threshold: 100, thresholdClass: 'bg-light-subtle', tooltip: 'Actual snowfall' },
-        { icon: 'bi bi-wind', label: 'Wind speed', value: this.wind, unit: this.wind_unit, threshold: 20, thresholdClass: 'bg-light-subtle', tooltip: 'Actual wind speed' },
-        { icon: 'bi bi-cloud-drizzle-fill', label: 'Rain', value: this.rain, unit: this.rain_unit, threshold: 20, thresholdClass: 'bg-info-subtle', tooltip: 'Actual rain' },
-        { icon: 'bi bi-bullseye', label: 'Visibility', value: this.visibility, unit: this.visibility_unit, threshold: 5000, thresholdClass: 'bg-success-subtle', tooltip: 'Actual visibility' },
-        { icon: 'bi bi-cloud-fill', label: 'Clouds', value: this.cloud, unit: this.cloud_unit, threshold: 80, thresholdClass: 'bg-secondary-subtle', tooltip: 'Actual cloud coverage' },
+        { icon: 'bi bi-brightness-high-fill', label: 'Temperature', value: null, unit: '', threshold: 17, thresholdClass: 'bg-warning-subtle', tooltip: 'Actual temperature' },
+        { icon: 'bi bi-thermometer-half', label: 'Temperature perception', value: null, unit: '', threshold: 17, thresholdClass: 'bg-warning-subtle', tooltip: 'Actual temperature perception' },
+        { icon: 'bi bi-droplet-half', label: 'Humidity', value: null, unit: '', threshold: 70, thresholdClass: 'bg-primary-subtle', tooltip: 'Actual humidity' },
+        { icon: 'bi bi-snow', label: 'Snowfall', value: null, unit: '', threshold: 100, thresholdClass: 'bg-light-subtle', tooltip: 'Actual snowfall' },
+        { icon: 'bi bi-wind', label: 'Wind speed', value: null, unit: '', threshold: 20, thresholdClass: 'bg-light-subtle', tooltip: 'Actual wind speed' },
+        { icon: 'bi bi-cloud-drizzle-fill', label: 'Rain', value: null, unit: '', threshold: 20, thresholdClass: 'bg-info-subtle', tooltip: 'Actual rain' },
+        { icon: 'bi bi-bullseye', label: 'Visibility', value: null, unit: '', threshold: 5000, thresholdClass: 'bg-success-subtle', tooltip: 'Actual visibility' },
+        { icon: 'bi bi-cloud-fill', label: 'Clouds', value: null, unit: '', threshold: 80, thresholdClass: 'bg-secondary-subtle', tooltip: 'Actual cloud coverage' },
       ],
     }
   },
@@ -56,23 +56,23 @@ export default {
       fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,rain,visibility,wind_speed_10m,snowfall,cloud_cover`)
           .then(response => response.json())
           .then(data => {
-            this.temperature = data.current['temperature_2m'];
-            this.wind = data.current['wind_speed_10m'];
-            this.ap_temperature = data.current['apparent_temperature'];
-            this.humidity = data.current['relative_humidity_2m'];
-            this.rain = data.current['rain'];
-            this.visibility = data.current['visibility'];
-            this.snow = data.current['snowfall'];
-            this.cloud = data.current['cloud_cover'];
+            this.weatherData[0].value = data.current['temperature_2m'];
+            this.weatherData[1].value = data.current['apparent_temperature'];
+            this.weatherData[2].value = data.current['relative_humidity_2m'];
+            this.weatherData[3].value = data.current['snowfall'];
+            this.weatherData[4].value = data.current['wind_speed_10m'];
+            this.weatherData[5].value = data.current['rain'];
+            this.weatherData[6].value = data.current['visibility'];
+            this.weatherData[7].value = data.current['cloud_cover'];
 
-            this.temperature_unit = data.current_units['temperature_2m'];
-            this.wind_unit = data.current_units['wind_speed_10m'];
-            this.ap_temperature_unit = data.current_units['apparent_temperature'];
-            this.humidity_unit = data.current_units['relative_humidity_2m'];
-            this.rain_unit = data.current_units['rain'];
-            this.visibility_unit = data.current_units['visibility'];
-            this.snow_unit = data.current_units['snowfall'];
-            this.cloud_unit = data.current_units['cloud_cover'];
+            this.weatherData[0].unit = data.current_units['temperature_2m'];
+            this.weatherData[1].unit = data.current_units['wind_speed_10m'];
+            this.weatherData[2].unit = data.current_units['apparent_temperature'];
+            this.weatherData[3].unit = data.current_units['relative_humidity_2m'];
+            this.weatherData[4].unit = data.current_units['rain'];
+            this.weatherData[5].unit = data.current_units['visibility'];
+            this.weatherData[6].unit = data.current_units['snowfall'];
+            this.weatherData[7].unit = data.current_units['cloud_cover'];
           })
           .catch(error => {
             console.error('Sorry, something is wrong, an error has occurred:', error);
@@ -101,16 +101,7 @@ export default {
        <p>Weather data for today.</p>
      </div>
      <div class="pt-4 row">
-       <WeatherCard
-           v-for="item in weatherData"
-           :key="item.label"
-           :icon="item.icon"
-           :label="item.label"
-           :value="item.value"
-           :unit="item.unit"
-           :threshold="item.threshold"
-           :thresholdClass="item.thresholdClass"
-           :tooltip="item.tooltip"
+       <WeatherCard v-for="item in weatherData" :key="item.label" :icon="item.icon" :label="item.label" :value="item.value" :unit="item.unit" :threshold="item.threshold" :thresholdClass="item.thresholdClass" :tooltip="item.tooltip"
        />
        <p><strong>Note:</strong> Current conditions are based on 15-minutely weather data.</p>
      </div>
